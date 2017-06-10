9a798d5c592d42070da36be5edad80fdc3e6bc69
sakochik <sakochi.kodai1209@gmail.com>
2017-06-11 04:11:12 +0900

    fix ui on favorite view controller & search view controller

---
+++ InstaClone/FavoriteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteTableViewCell.swift
+//  InstaClone
+//
+//  Created by 迫地康大 on 2017/06/11.
+//  Copyright © 2017年 sakochi. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # InstaClone
 
-主に以下のことを学ぶ目的でサンプルアプリを作ります
-・画面遷移&画面間でのデータの受け渡し
-・TableView(&TableViewCell)の作成
-・カメラ、アルバムの利用
-・Firebase Realtime Databaseへの接続
+<p>主に以下のことを学ぶ目的でサンプルアプリを作ります</p>
+<p>
+・画面遷移&画面間でのデータの受け渡し<br>
+・TableView(&TableViewCell)の作成<br>
+・カメラ、アルバムの利用<br>
+・Firebase Realtime Databaseへの接続<br>
+・お気に入り機能の設定
+</p>
